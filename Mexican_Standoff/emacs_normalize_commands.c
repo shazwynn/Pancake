b9e78d63a12081d6675ffcf8c51f195b130a5f17
42Skwyrl <agrele@student.42.fr>
2015-12-06 06:59:08 +0100

    brouillon emacs normalize '

---
+++ Mexican_Standoff/emacs_normalize_commands.c
@@ -0,0 +1,24 @@
+
+
+// cut and paste var declarations
+M-g 5 enter || C-u 4 down
+C-u 3 C-k
+haut haut C-y
+
+// espace apres le while
+M-f espace
+
+// espace apres break
+C-s k enter space
+
+// remove trailing whitespaces
+down down down C-u 4 C-d enter
+
+
+// Header
+
+// Indent : must be tabs not spaces
+C-x h Tab
+
+// Save
+C-x C-c
